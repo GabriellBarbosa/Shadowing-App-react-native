01ba43fef13c6e2d6e149655c8b0c837c98f1127
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-11-06 21:11:34 -0300

    list audios in the right sequence

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="audio/[id]" />
+      <Stack.Screen name="audio/[id]" options={{ headerTitle: 'Home' }} />
     </Stack>
   );
 }
--- app/audio/[id].tsx
+++ app/audio/[id].tsx
@@ -1,18 +1,54 @@
 import React from "react";
-import { Text } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { Audio } from 'expo-av';
 
 export default function AudioScreen() {
+    const [audios, setAudios] = React.useState<any[]>([]);
     const { id } = useLocalSearchParams();
 
     React.useEffect(() => {
     fetch(`http://127.0.0.1:5000/audio/${id}`)
         .then((res) => res.json())
-        .then((json) => console.log(json))
+        .then(async (audios) => {
+            const loadedAudios = await loadAudios(audios);
+            setAudios(loadedAudios);
+        })
         .catch((err) => console.error(err));
     }, []);
+    
+    async function loadAudios(audiosUrl: any[]) {
+        const result = [];
+        for await (const url of audiosUrl) {
+            const loaded = await Audio.Sound.createAsync({
+                uri: `http://127.0.0.1:5000/${url}`,
+            });
+            result.push(loaded.sound);
+        }
+        return result;
+    }
 
     return (
-        <Text>Hello my dear world!</Text>
+        <View style={styles.container}>
+            {audios && audios.map((audio, index) => (
+                <View key={index} style={styles.buttons}>
+                    <Button
+                        onPress={async () => await audio.playAsync()}
+                        title={`Play Audio`}
+                    />
+                </View>
+            ))}
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        gap: 20
+    },
+    buttons: {
+        display: 'flex',
+        gap: 1
+    }
+})
