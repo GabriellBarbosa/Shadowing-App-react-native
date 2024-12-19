a23dec2869659f167ef7450dddcc5ab491deab7f
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-12-19 16:20:04 -0300

    download audio from youtube

--- components/FileUploadComponent.tsx
+++ components/FileUploadComponent.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
 import { Button, View } from 'react-native';
 import { SERVER_HOST } from '@/utils/constants';
-import * as DocumentPicker from 'expo-document-picker';
 
 interface Props {
   onUploadComplete: () => void;
@@ -11,52 +10,23 @@ export default function FileUploadComponent(props: Props) {
   const [loading, setLoading] = React.useState<boolean>(false);
 
   const pickFileAndUpload = useCallback(async () => {
-    const file = await pickFile();
-    if (file != null)
-      await uploadFile(file);
-  }, []);
-
-  async function pickFile() {
-    try {
-      return await tryToPick();
-    } catch (err) {
-      console.error(`Error occurred on file pick: ${err}`);
-      return null;
-    }
-  }
-
-  async function tryToPick() {
-    const result = await DocumentPicker.getDocumentAsync({
-      type: 'audio/x-wav',
-    });
-    return result.assets ? result.assets[0] : null;
-  }
-
-  async function uploadFile(file: DocumentPicker.DocumentPickerAsset) {
     try {
       setLoading(true);
-      await tryToUpload(file);
-      props.onUploadComplete();
+      await tryToUpload();
     } catch(err) {
       console.error(`error occurred on file upload: ${err}`);
     } finally {
       setLoading(false);
     }
-  }
+  }, []);
 
-  async function tryToUpload(file: DocumentPicker.DocumentPickerAsset) {
-    await fetch(`${SERVER_HOST}/upload`, {
+  async function tryToUpload() {
+    await fetch(`${SERVER_HOST}/yt`, {
       method: 'POST',
-      body: createFormData(file)
+      body: JSON.stringify({ url: "https://youtu.be/Q6dKCG5tYOY?feature=shared" }),
+      headers: { 'content-type': 'application/json' }
     })
-    .catch((err) => console.error(err));
-  }
-
-  function createFormData({ name, uri, mimeType }: DocumentPicker.DocumentPickerAsset) {
-    const formData = new FormData();
-    const fileData = JSON.stringify({name, uri, type: mimeType})
-    formData.append('file', JSON.parse(fileData));
-    return formData;
+    props.onUploadComplete();
   }
 
   return (
