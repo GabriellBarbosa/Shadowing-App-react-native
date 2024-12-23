c64e7ddeeef938326b86b425efd24022bc941bcb
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-12-23 15:03:23 -0300

    download video from Youtube

--- components/FileUploadComponent.tsx
+++ components/FileUploadComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, TextInput, StyleSheet, Text } from 'react-native';
 import { SERVER_HOST } from '@/utils/constants';
 
 interface Props {
@@ -8,34 +8,55 @@ interface Props {
 
 export default function FileUploadComponent(props: Props) {
   const [loading, setLoading] = React.useState<boolean>(false);
+  const [videoUrl, setVideoUrl] = React.useState<string>('');
 
-  const pickFileAndUpload = useCallback(async () => {
+  async function downloadAudio() {
     try {
       setLoading(true);
       await tryToUpload();
+      props.onUploadComplete();
+      setVideoUrl('');
     } catch(err) {
       console.error(`error occurred on file upload: ${err}`);
     } finally {
       setLoading(false);
     }
-  }, []);
+  };
 
   async function tryToUpload() {
     await fetch(`${SERVER_HOST}/yt`, {
       method: 'POST',
-      body: JSON.stringify({ url: "https://youtu.be/Q6dKCG5tYOY?feature=shared" }),
+      body: JSON.stringify({ url: videoUrl }),
       headers: { 'content-type': 'application/json' }
     })
-    props.onUploadComplete();
   }
 
   return (
     <View>
+      <TextInput 
+        placeholder='Please paste the YouTube video URL'
+        style={styles.input}
+        value={videoUrl}
+        onChange={(e) => setVideoUrl(e.nativeEvent.text)}
+        placeholderTextColor="#6c757d"
+      />
       {loading ? (
-        <Button title="It may take some minutes..." color="#02c39a" />
+        <Button title="It may take a few minutes..." color="#02c39a" />
       ) : (
-        <Button title="Upload Audio" onPress={pickFileAndUpload} color="#02c39a" />
+        <Button title="Download Audio" onPress={downloadAudio} color="#02c39a" />
       )}
     </View>
   );
-  };
+};
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    borderWidth: 1,
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    borderColor: '#6c757d',
+    color: '#fff'
+  },
+});
